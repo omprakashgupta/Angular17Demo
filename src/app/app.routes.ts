d9140d6daed2da5d336cd88a8fff6889c5bd1835
OM Gupta <om.siplservices.com>
2025-03-31 08:29:20 +0530

    1. RxJS testing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ImageMarkingComponent } from './image-marking/image-marking.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 export const routes: Routes = [
-    {path: '', component: ImageMarkingComponent}
+    // {path: '', component: ImageMarkingComponent}
+    {path: '', component: RxjsComponent}
 ];
