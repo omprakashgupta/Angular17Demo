bbad1867b718aa145edd229e0671448a0bf88f81
OM Gupta <om.siplservices.com>
2025-07-09 01:12:40 +0530

    1. Mock intercepror is added to simulate api call.

---
+++ src/app/mock-interceptor-test/mock-interceptor-test.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mock-interceptor-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './mock-interceptor-test.component.html',
+  styleUrl: './mock-interceptor-test.component.css'
+})
+export class MockInterceptorTestComponent {
+
+}
